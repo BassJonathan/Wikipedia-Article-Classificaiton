828fadf4e49cb8a98f56ecfd08ff7c6814b461bf
Jonathan Bass <44204074+BassJonathan@users.noreply.github.com>
2023-07-14 15:44:30 +0200

    Add: Restructure Repo & Add Rule-Based Classification Notebook

--- bin/download-and-unzip-index.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ../Data
-
-curl https://dumps.wikimedia.org/dewiki/latest/dewiki-latest-pages-articles-multistream-index.txt.bz2 > ../Data/dewiki-latest-pages-articles-multistream-index.txt.bz2
-
-lbzip2 -d ../Data/dewiki-latest-pages-articles-multistream-index.txt.bz2
